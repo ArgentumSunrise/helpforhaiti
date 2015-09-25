40586d38c37909475329f0ab4cdcc658159344a6
ArgentumSunset <retrieverofdestiny@gmail.com>
2015-09-24 21:46:02 -0600

    Added navbar, script for nav elements

---
+++ controller.js
@@ -0,0 +1,25 @@
+//var app = angular.module('migrantsApp', ['ngRoute']);
+//
+//app.config(['$routeProvider', function ($routeProvider) {
+//    $routeProvider.
+//    when('/', {
+//        templateUrl: 'partials/home.html',
+//        controller: 'weatherCtrl',
+//    }).
+//     when('/forecast', {
+//        templateUrl: 'partials/mission.html',
+//        controller: 'weatherCtrl',
+//    }).
+//     when('/forecast', {
+//        templateUrl: 'partials/plan.html',
+//        controller: 'weatherCtrl',
+//    }).
+//    when('/forecast', {
+//        templateUrl: 'partials/about.html',
+//        controller: 'weatherCtrl',
+//    }).
+//    otherwise({
+//        redirectTo: '/',
+//    });
+//
+//}]);     When we've designed everything, then we'll do the routing, okay?
